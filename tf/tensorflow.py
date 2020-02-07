75a7a9960a8f8d06c33a641bd720811b16d05ff1
Timz Owen <ktimothy@kabarak.ac.ke>
2020-02-07 11:24:24 +0300

    Update tensorflow.py
    updated final training, testing and model predictions

--- tf/tensorflow.py
+++ tf/tensorflow.py
@@ -55,3 +55,46 @@ def label_delineator(df_train, df_test, label):
 train_data, train_labels, test_data, test_labels = label_delineator(df_train, df_test, 'isLegendary')
 
 #upnext is normalizing the data so that each pikachu is on the same scale
+def data_normalizer(train_data, test_data):
+    train_data = preprocessing.MinMaxScaler().fit_transform(train_data)
+    test_data = preprocessing.MinMaxScaler().fit_transform(test_data)
+    return(train_data, test_data)
+
+train_data, test_data = data_normalizer(train_data, test_data)
+
+#Now on Machine Learning , we will be using Keras API 
+#we wil; build our model in 2layers .
+#user Rectified linear Unit and Logistic regression on our model to 2 probability groups and labels 
+length = train_data.shape[1]
+
+model = keras.Sequential()
+model.add(keras.layers.Dense(500, activation='relu', input_shape=[length,]))
+model.add(keras.layers.Dense(2, activation='softmax'))
+
+
+#compiling, evaluating and fitting data into the model 
+#pick an optimizer and loss to show the training rate
+#I used the SGD Algorithim  fro optimization
+model.compile(optimizer='sgd', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+#now fit in the model with Training data, training label ad epoch . as it is supervised Machine Learning;
+model.fit(train_data, train_labels, epochs=400)
+
+#now that  the model is trained, lets test with oue test data 
+loss_value, accuracy_value = model.evaluate(test_data, test_labels)
+print(f'Our test accuracy was {accuracy_value})'
+>>> Our test accuracy was 0.980132 which is not tha bod, almost perfect accuracy
+
+#now predict a given pokemon  with model.predict
+def predictor(test_data, test_labels, index):
+    prediction = model.predict(test_data)
+    if np.argmax(prediction[index]) == test_labels[index]:
+        print(f'This was correctly predicted to be a \"{test_labels[index]}\"!')
+    else:
+        print(f'This was incorrectly predicted to be a \"{np.argmax(prediction[index])}\". It was actually a \"{test_labels[index]}\".')
+        return(prediction)
+        
+#Let's look at one of the more well-known legendary Pokémon: Mewtwo.
+#  He's number 150 in the list of Pokémon, so we'll look at index 149:
+
+predictor(test_data, test_labels, 149)
+>>> This was correctly predicted to be a "1"!
