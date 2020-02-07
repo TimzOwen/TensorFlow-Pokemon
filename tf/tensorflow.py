7ce8cbdcd256aa3e299c3b0e4dc6d49a0f21bbe8
Timz Owen <ktimothy@kabarak.ac.ke>
2020-02-07 10:17:04 +0300

    Update tensorflow.py
    splitting and normalization of datasets

--- tf/tensorflow.py
+++ tf/tensorflow.py
@@ -28,3 +28,30 @@ def dummy_creation(df, dummy_categories):
 df = dummy_creation(df, ['Egg_Group_1', 'Body_Style', 'Color','Type_1', 'Type_2'])
 
 #Upnext Splitting and normalizing data
+#we will split our data into training and testing data, Let's use pekomen generation of
+#blue, red and yellow fro training
+def train_test_splitter(DataFrame, column):
+    df_train = DataFrame.loc[df[column] != 1]
+    df_test = DataFrame.loc[df[column] == 1]
+
+    df_train = df_train.drop(column, axis=1)
+    df_test = df_test.drop(column, axis=1)
+
+    return(df_train, df_test)
+
+df_train, df_test = train_test_splitter(df, 'Generation')
+
+# separate labels from the data sets as category 1 and the rest
+def label_delineator(df_train, df_test, label):
+    
+    train_data = df_train.drop(label, axis=1).values
+    train_labels = df_train[label].values
+    test_data = df_test.drop(label,axis=1).values
+    test_labels = df_test[label].values
+    return(train_data, train_labels, test_data, test_labels)
+
+#extract the data from the datasets and out them into arrays which tensorflow can understand
+#usinf .values
+train_data, train_labels, test_data, test_labels = label_delineator(df_train, df_test, 'isLegendary')
+
+#upnext is normalizing the data so that each pikachu is on the same scale
